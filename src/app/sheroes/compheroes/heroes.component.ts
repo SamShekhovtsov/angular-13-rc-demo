08e174d96636b58927d92fbde180894d92235231
SamShekhovtsov <semshehovtsov@gmail.com>
2021-11-02 03:54:57 +0200

    1 file modified, 1 added

--- src/app/sheroes/compheroes/heroes.component.ts
+++ src/app/sheroes/compheroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,30 +11,29 @@ import { HeroService } from '../hero.service';
   //styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   selectedHero?: Hero;
-  sub_title = "List of Sems Heroes"
-  heroes: Hero[] = []
+  sub_title = 'List of Sems Heroes';
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
-  //constructor(private heroService: HeroService, private messageService: MessageService) { }
-  //constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.getHeroes();
   }
 
   //ngAfterViewInit() {
-    //this.getHeroes();
+  //this.getHeroes();
   //}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    //this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
